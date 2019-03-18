61c12b55ce83c13d8e73f4bfe8f884511ab2d5cf
yanzw <yanwei416@163.com>
2019-03-18 20:31:42 +0800

    修改vant引入问题

--- src/main.js
+++ src/main.js
@@ -1,31 +1,36 @@
-//导入所需模块
+// 导入所需模块
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
 Vue.config.productionTip = false
 
-//ajax
+// ajax
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 Vue.use(VueAxios, axios)
 
-//状态管理
+// 状态管理
 import vuex from 'vuex'
+
 Vue.use(vuex);
 
-//ui 框架
+// ui 框架
 import Vant from 'vant';
-import 'vant/lib/vant-css/index.css';
+// import 'vant/lib/vant-css/index.css';
+import 'vant/lib/index.css';
 
 Vue.use(Vant);
 
-//懒加载
-import { Lazyload } from 'vant';
+// 懒加载
+import {Lazyload} from 'vant';
+
 Vue.use(Lazyload);
 
-//瀑布流
-import { Waterfall } from 'vant';
+// 瀑布流
+import {Waterfall} from 'vant';
+
 Vue.use(Waterfall);
 
 import store from './store/index'
@@ -35,6 +40,6 @@ new Vue({
   el: '#app',
   router,
   store,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
